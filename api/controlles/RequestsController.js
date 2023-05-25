b94f0e2b37d8461a9c9cd1362f7086e88a24e082
viny1290 <vinijudo532@gmail.com>
2023-05-25 19:27:14 -0300

    apiQuick

--- api/controlles/RequestsController.js
+++ api/controlles/RequestsController.js
@@ -9,6 +9,52 @@ class RequestsController{
             return res.status(500).json(error.message);
         }
     }
+
+    static async pegaUmRequests(req, res){
+        const {id} = req.params
+        try{
+            const umRequests = await database.requests.findOne( {
+                where: { id: Number(id) }
+            });
+            return res.status(200).json(umRequests);
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async criaRequests(req, res){
+        const novoRequests = req.body;
+        try{
+            const novoRequestsCriado = await database.requests.create(novoRequests);
+            return res.status(200).json(novoRequestsCriado);
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async atualizaRequests(req, res){
+        const {id} = req.params;
+        const novasInfos = req.body;
+        try{
+            await database.requests.update(novasInfos, { where: { id: Number(id) } });
+            const requestsAtualizada = await database.requests.findOne( {
+                where: { id: Number(id) }
+            });
+            return res.status(200).json(requestsAtualizada);
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async apagaRequests(req, res){
+        const{ id } = req.params;
+        try{
+            await database.requests.destroy({ where: { id: Number(id) } });
+            return res.status(200).json({ mensagem: `id ${id} deletado.` });
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = RequestsController;
--- api/routes/requestsRoute.js
+++ api/routes/requestsRoute.js
@@ -4,5 +4,9 @@ const RequestsController = require('../controlles/RequestsController');
 const router = Router();
 
 router.get('/requests', RequestsController.pegaTodosOsRequests);
+router.get('/requests/:id', RequestsController.pegaUmRequests);
+router.post('/requests', RequestsController.criaRequests);
+router.put('/requests/:id', RequestsController.atualizaRequests);
+router.delete('/requests/:id', RequestsController.apagaRequests);
 
 module.exports = router;
